02360937362d788a40342a68de2632ebd1042719
koftov <ahronlu@gmail.com>
2020-12-09 19:57:46 +0200

    streamReducer finito

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,13 +1,11 @@
+import _ from "lodash";
 import {
   CREATE_STREAM,
   DELETE_STREAM,
   EDIT_STREAM,
   FETCH_STREAM,
+  FETCH_STREAMS,
 } from "../actions/types";
-
-const INITIAL_STATE = {
-  streams: {},
-};
 
 export const streamReducer = (state = {}, action) => {
   switch (action.type) {
@@ -15,9 +13,10 @@ export const streamReducer = (state = {}, action) => {
     case CREATE_STREAM:
     case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
-
     case DELETE_STREAM:
-      return { ...state };
+      return _.omit(state, action.payload);
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
     default:
       return state;
   }
