5ec94459236eee676b1ec7c4a96356cea7524e05
koftov <ahronlu@gmail.com>
2020-12-09 20:08:22 +0200

    rendering all streams

--- client/src/Components/streams/StreamList.js
+++ client/src/Components/streams/StreamList.js
@@ -1,16 +1,32 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Icon, Item, List } from "semantic-ui-react";
 import { fetchStreams } from "../../actions";
 
 const StreamList = () => {
   const dispatch = useDispatch();
 
-  const streams = useSelector((state) => state.streams);
+  const streams = useSelector((state) => Object.values(state.streams));
 
   useEffect(() => {
     dispatch(fetchStreams());
   }, [dispatch]);
-  return <div></div>;
+  return (
+    <div>
+      <h2>Streams</h2>
+      <List celled>
+        {streams.map((stream) => (
+          <Item key={stream.id}>
+            <Icon name="camera" size="large" className="middle aligned" />
+            <Item.Content>
+              {stream.title}
+              <Item.Description>{stream.description}</Item.Description>
+            </Item.Content>
+          </Item>
+        ))}
+      </List>
+    </div>
+  );
 };
 
 export default StreamList;
