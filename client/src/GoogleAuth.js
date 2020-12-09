44fbee8d6d887628cc2f7b7a03f040292e0fafd3
koftov <ahronlu@gmail.com>
2020-12-09 09:59:28 +0200

    google auth

--- client/src/GoogleAuth.js
+++ client/src/GoogleAuth.js
@@ -1,23 +1,60 @@
-import React, { useEffect } from "react";
+import React, { Component } from "react";
 import { Button, Icon } from "semantic-ui-react";
 
-const GoogleAuth = () => {
-  useEffect(() => {
-    window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "579900251248-m92e98qadl4polj9k53qtdhrkgtic0le.apps.googleusercontent.com",
-        scope: "email",
-      });
-    });
-  });
+class GoogleAuth extends Component {
+  state = { isSignedIn: null };
 
-  return (
-    <Button color="red">
-      <Icon name="google" />
-      Login with Google
-    </Button>
-  );
-};
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "579900251248-m92e98qadl4polj9k53qtdhrkgtic0le.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChage);
+        });
+    });
+  }
+
+  onAuthChage = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
+  renderAuthButton = () => {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <Button onClick={this.onSignOut} color="red">
+          <Icon name="google" />
+          Sign Out
+        </Button>
+      );
+    } else {
+      return (
+        <Button onClick={this.onSignIn} color="red">
+          <Icon name="google" />
+          Sign In
+        </Button>
+      );
+    }
+  };
+
+  render() {
+    return <>{this.renderAuthButton()}</>;
+  }
+}
 
 export default GoogleAuth;
