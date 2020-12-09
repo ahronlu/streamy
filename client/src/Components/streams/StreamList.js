2543cd0031967e8bd62a8ba79cb77799378440bc
koftov <ahronlu@gmail.com>
2020-12-09 20:18:38 +0200

    conditianally showing edit and delete

--- client/src/Components/streams/StreamList.js
+++ client/src/Components/streams/StreamList.js
@@ -1,22 +1,36 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Icon, Item, List } from "semantic-ui-react";
+import { Button, Icon, Item, List } from "semantic-ui-react";
 import { fetchStreams } from "../../actions";
 
 const StreamList = () => {
   const dispatch = useDispatch();
 
   const streams = useSelector((state) => Object.values(state.streams));
+  const auth = useSelector((state) => state.auth);
+  console.log(auth);
 
   useEffect(() => {
     dispatch(fetchStreams());
   }, [dispatch]);
+
+  const renderAdmin = (stream) => {
+    if (stream.userId === auth.userId) {
+      return (
+        <div className="right floated content">
+          <Button primary>Edit</Button>
+          <Button negative>Delete</Button>
+        </div>
+      );
+    }
+  };
   return (
     <div>
       <h2>Streams</h2>
       <List celled>
         {streams.map((stream) => (
           <Item key={stream.id}>
+            {renderAdmin(stream)}
             <Icon name="camera" size="large" className="middle aligned" />
             <Item.Content>
               {stream.title}
