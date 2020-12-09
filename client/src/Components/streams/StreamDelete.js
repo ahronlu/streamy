65529a56498e01e8ef05f008db035de22d2f970c
koftov <ahronlu@gmail.com>
2020-12-09 21:42:55 +0200

    deleteStram

--- client/src/Components/streams/StreamDelete.js
+++ client/src/Components/streams/StreamDelete.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { Button, Modal } from "semantic-ui-react";
+import { deleteStream } from "../../actions";
+import history from "../../history";
 
-const StreamDelete = () => {
-  return <div>d</div>;
+const StreamDelete = ({ match }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteStream(match.params.id));
+  };
+
+  const goBack = () => {
+    history.push("/");
+  };
+
+  return (
+    <Modal open onClose={goBack}>
+      <Modal.Header>Delete Stream</Modal.Header>
+      <Modal.Content>
+        Are you sure you want to delete this stream?
+      </Modal.Content>
+      <Modal.Actions>
+        <Button primary onClick={handleDelete}>
+          Delete
+        </Button>
+        <Button onClick={goBack}>Cancel</Button>
+      </Modal.Actions>
+    </Modal>
+  );
 };
 
 export default StreamDelete;
